18b27796c0f2c4616ee0566c87b9ac3b0447e0c9
Chidike Divine <chidikedvine@gmail.com>
2022-07-16 01:37:48 +0100

    add all

--- index.php
+++ index.php
@@ -14,6 +14,8 @@ session_start();
     <link href='https://unpkg.com/boxicons@2.1.2/css/boxicons.min.css' rel='stylesheet'>
     <link href="https://fonts.googleapis.com/icon?family=Material+Icons"
       rel="stylesheet">
+      <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css">
+    <script src="https://unpkg.com/scrollreveal"></script>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>bloodbank</title>
 </head>
