d95a57b7d60fc44147a6342a2030b03068a886fe
Chidike Divine <chidikedvine@gmail.com>
2022-07-25 21:21:39 +0100

    all

--- login.php
+++ login.php
@@ -162,7 +162,19 @@ session_start();
     }
     </script>
 
-        <script src="/js/slide.js"></script>
+        <script>
+const sign_prsn = document.querySelector("#sign-prsn");
+const sign_hsptl = document.querySelector("#sign-hsptl");
+const page = document.querySelector(".page")
+
+sign_hsptl.addEventListener('click', () => {
+    page.classList.add("sign-up-mode");
+});
+sign_prsn.addEventListener('click', () => {
+    page.classList.remove("sign-up-mode");
+});
+
+        </script>
 
 
 </body>
